5d27139036e38116ce3a2b66632d882f7e5624b7
pjcbakx <pjcbakx@avans.nl>
2015-09-09 14:03:26 +0200

    Added LocationData and CloclTime classes for information of the Bars

---
+++ Happyhour/ClockTime.cs
@@ -0,0 +1,60 @@
+﻿public class ClockTime
+{
+    public int hour;
+    public int minutes;
+    public int day;
+
+    public ClockTime()
+    {
+        hour = 0;
+        minutes = 0;
+        int day = 0;
+    }
+
+    public ClockTime(int hour, int minutes, int day)
+    {
+        this.hour = hour;
+        this.minutes = minutes;
+        this.day = day;
+    }
+
+    public string getDay()
+    {
+        switch (day)
+        {
+            case 0:
+                return "Monday";
+            case 1:
+                return "Tuesday";
+            case 2:
+                return "Wednesday";
+            case 3:
+                return "Thursday";
+            case 4:
+                return "Friday";
+            case 5:
+                return "Saterday";
+            case 6:
+                return "Sunday";
+        }
+        return "";
+    }
+
+    public string  getString()
+    {
+        string hourString;
+        string minutesString;
+
+        if (hour < 10)
+            hourString = "0" + hour.ToString();
+        else
+            hourString = hour.ToString();
+
+        if (minutes < 10)
+            minutesString = "0" + minutes.ToString();
+        else
+            minutesString = "0" + minutes.ToString();
+
+        return (hourString + ":" + minutesString);
+    }
+}
