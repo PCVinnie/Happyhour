dae89b77affa3cd838818caf6fd183073e564f2f
pjcbakx <pjcbakx@avans.nl>
2015-09-16 13:58:37 +0200

    Finshed read XML file function

--- Happyhour/Model/XMLFileReader.cs
+++ Happyhour/Model/XMLFileReader.cs
@@ -62,11 +62,11 @@ namespace Happyhour
                                 Double.TryParse(element.Value.ToString(), out location.rating);
                                 break;
                             case "OPENTIMES":
-                                element = XElement.ReadFrom(reader) as XElement;
+                                readTimes(reader, true, location);
                                 
                                 break;
                             case "CLOSETIMES":
-                                element = XElement.ReadFrom(reader) as XElement;
+                                readTimes(reader, false, location);
                                 break;
                             case "LONGITUDE":
                                 element = XElement.ReadFrom(reader) as XElement;
@@ -85,9 +85,97 @@ namespace Happyhour
             return locations;
         }
 
-        private void readTimes(XmlReader reader)
+        private void readTimes(XmlReader reader, Boolean isOpentime, LocationData location)
         {
+            ClockTime time = new ClockTime();
+            XElement element;
+            string typeTime = "CLOSETIMES";
 
+            if (isOpentime)
+                typeTime = "OPENTIMES";
+
+            while (reader.Read() && reader.NodeType != XmlNodeType.EndElement && reader.Name.ToString().ToUpper() != typeTime)
+            {
+                if (reader.IsStartElement())
+                {
+                    time = new ClockTime();
+                    switch (reader.Name.ToString().ToUpper())
+                    {
+                        case "MONDAY":
+                            time.day = 0;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+                        case "TUESDAY":
+                            time.day = 1;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+                        case "WEDNESDAY":
+                            time.day = 2;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+                        case "THURSDAY":
+                            time.day = 3;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            break;
+                        case "FRIDAY":
+                            time.day = 4;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+                        case "SATERDAY":
+                            time.day = 5;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+                        case "SUNDAY":
+                            time.day = 6;
+                            element = XElement.ReadFrom(reader) as XElement;
+                            FillHoursAndMinutes(time, element);
+                            if (isOpentime)
+                                location.addOpenTime(time);
+                            else
+                                location.addCloseTime(time);
+                            break;
+
+                    }
+                }
+            }
+        }
+
+        private void FillHoursAndMinutes(ClockTime time, XElement element)
+        {
+            string hoursminutes = element.Value.ToString();
+            if (hoursminutes == "closed")
+                time.closed = true;
+            else
+            {
+                Int32.TryParse((hoursminutes[0].ToString() + hoursminutes[1].ToString()), out time.hour);
+                Int32.TryParse((hoursminutes[2].ToString() + hoursminutes[3].ToString()), out time.minutes);
+            }
         }
     }
 }
