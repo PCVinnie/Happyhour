eca4265372fcb080c1b9923aac19ae7b22cb8afb
PCVinnie <vastout@vasnet.nl>
2015-09-19 15:00:51 +0200

    Added bar, happyhour and settings to project

--- Happyhour/View/HappyHour.xaml.cs
+++ Happyhour/View/HappyHour.xaml.cs
@@ -12,30 +12,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using View;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Happyhour
+namespace Happyhour.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class HappyHour : Page
     {
-        public MainPage()
+        public HappyHour()
         {
             this.InitializeComponent();
-        }
-
-        private void Pub_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Route_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Map));
         }
     }
 }
