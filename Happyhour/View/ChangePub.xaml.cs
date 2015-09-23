7b9d2c4a6187a91baf40eb4aeb72e71e78f0c7ce
PCVinnie <vastout@vasnet.nl>
2015-09-23 13:29:23 +0200

    Fixed some form bugs

--- Happyhour/View/ChangePub.xaml.cs
+++ Happyhour/View/ChangePub.xaml.cs
@@ -27,6 +27,52 @@ namespace Happyhour.View
             this.InitializeComponent();
         }
 
+        public void inputData()
+        {
+            string name = Name_TextBox.Text;
+            string street = Street_TextBox.Text;
+            string houseNumber = Housenumber_TextBox.Text;
+            string zipCode = Zipcode_TextBox.Text;
+            string place = Place_TextBox.Text;
+            string country = Country_TextBox.Text;
+            string openinghours = Openinghours_TextBox.Text;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen naam opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(street))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen straat opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(houseNumber))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen huisnummer opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(zipCode))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen postcode opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(place))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen plaats opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(country))
+            {
+                ErrorMessage_TextBlock.Text = "Er is geen land opgegeven.";
+            }
+            else if (string.IsNullOrEmpty(openinghours))
+            {
+                ErrorMessage_TextBlock.Text = "Er zijn geen openingstijden opgegeven.";
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            inputData();
+        }
+
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(View.AppSettings));
